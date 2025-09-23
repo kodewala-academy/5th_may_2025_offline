7a1a16ab761279a05e8ca6da107b4b07af6ab750
kodewala-academy <kodewala.academy@gmail.com>
2025-09-23 13:33:49 +0530

    ..

---
+++ SpringWebMVCModelAttribute/src/main/java/com/zepto/entity/UserDetails.java
@@ -0,0 +1,66 @@
+package com.zepto.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class UserDetails {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int userId;
+	@Column
+	private String userName;
+	@Column
+	private String firstName;
+	@Column
+	private String lastName;
+	@Column
+	private String status;
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+}
